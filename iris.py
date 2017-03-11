73c4c43d8c25f419540d9bbbefb3447074067168
Adithya J <adithya.j@adithya321.me>
2017-03-11 20:21:31 +0530

    :sparkles: Export Decision Tree in pdf format

--- iris.py
+++ iris.py
@@ -19,3 +19,22 @@ clf = clf.fit(train_data, train_target)
 
 print test_target
 print clf.predict(test_data)
+
+# viz code
+from sklearn.externals.six import StringIO
+import pydotplus
+
+dot_data = StringIO()
+tree.export_graphviz(clf,
+                     out_file=dot_data,
+                     feature_names=iris.feature_names,
+                     class_names=iris.target_names,
+                     filled=True, rounded=True,
+                     impurity=False)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("iris.pdf")
+
+print test_data[2], test_target[2]
+
+print iris.feature_names
+print iris.target_names
